21f6238da695a3d1ecfd74e8c5fff5e184fe1ce8
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2024-11-05 21:37:06 -0500

    intalled tailwinds

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import iAddToCart from "./assets/images/icon-add-to-cart.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header bg-yellow">
                 <img src={iAddToCart} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
-                    className="App-link"
+                    className="App-link p-2"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
