a42b1d1c8a58f0c1e52a38ae416747a8f43d3c88
Molly <117697419+learningcoding2022@users.noreply.github.com>
2024-11-14 21:50:46 -0500

    changes to text and button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-//import iAddToCart from "./images/icon-add-to-cart.svg";
 import "./App.css";
 import "./index.css";
-import ExampleComponent from "./molly.js";
+import DessertComponent from "./molly.js";
 
 
 function App() {
     return (
         <div className="App">
             <header className="App-header bg-lightPurple">
-                {/* <img src={iAddToCart} className="App-logo" alt="logo" /> */}
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link p-2"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <ExampleComponent/>
+                <DessertComponent/> 
             </header>
         </div>
     );
--- src/molly.js
+++ src/molly.js
@@ -3,14 +3,21 @@ import data from '../src/items-list/data.json';
 // import images from './images/image-waffle-thumbnail.jpg';
 
 
-const ExampleComponent = () => {
+const DessertComponent = () => {
   return (
     <div>
-      <h2>User List</h2>
+      <h2>Dessert</h2>
       <ul>
         {data.map((user) => (
           <li key={user.id}> 
           <img src={user.image.mobile} alt={user.name}/>
+            <button className='bg-orange rounded-full w-[200px] p-2 '>
+              <img src={`${process.env.PUBLIC_URL}/images/icon-add-to-cart.svg`}
+              alt="Icon" class="w-6 h-6 mr-2"/>
+              <span>Add to Cart</span>
+            </button><br></br>
+
+            {user.category}<br></br>
             {user.name} - $ {user.price}
           </li>
         ))}
@@ -26,4 +33,4 @@ const ExampleComponent = () => {
   );
 };
 
-export default ExampleComponent;
+export default DessertComponent;
